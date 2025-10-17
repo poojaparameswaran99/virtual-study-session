0d8ccb78612dfc3e57353a4f06d12ee8a793f0be
poojaparameswaran99 <poojaparameswaran99@github.com>
2025-10-17 11:29:00 -0400

    bash script to submit

---
+++ grant_review_process/meet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=cellbio-dgx # partition
+#SBATCH --account=soderlinglab
+#SBATCH --job-name=meet    # job -name , change from command line 
+#SBATCH --gres=gpu:1
+#SBATCH --mem=200G
+#SBATCH --time=12-00:00:00 # you have asked for 12 days
+#SBATCH --output=slurm_output/%x.%j.out # Standard output log, %x is the job name, %j is the job ID, y is custom time stamp
+#SBATCH --error=slurm_output/%x.%j.err      # Standard error log, %x is the job name, %j is the job ID
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=pooja.parameswaran@duke.edu
+
+echo Job Name: meet
+
+
+export TORCH_HOME='/cwork/pkp14'
+python run_study_session.py "$@"
